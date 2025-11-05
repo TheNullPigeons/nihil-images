fa3c6b6620d8802ce5cb79d792b4d362755856e9
0xbbuddha <bouddha.whitehat@gmail.com>
2025-11-05 11:03:00 +0100

    docs(readme): fix run command typo and document new structure

---
+++ runtime/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Nihil Project
+# Runtime entrypoint: keep container alive or execute a command
+
+set -e
+
+trap "exit 0" SIGTERM SIGINT
+
+run_default() {
+    exec tail -f /dev/null
+}
+
+run_cmd() {
+    shift
+    exec "$@"
+}
+
+if [ $# -eq 0 ]; then
+    run_default
+else
+    if [ "$1" = "cmd" ]; then
+        run_cmd "$@"
+    else
+        exec "$@"
+    fi
+fi
