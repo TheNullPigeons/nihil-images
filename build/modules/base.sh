592a6b76a90d5f160d01e45b9be83dc1457e49da
0xbbuddha <killian@archimedeos.org>
2026-01-22 20:19:36 +0100

    ajout python-pip, wheel

--- build/modules/base.sh
+++ build/modules/base.sh
@@ -8,7 +8,7 @@ source "${SCRIPT_DIR}/../lib/common.sh"
 function package_base() {
     colorecho "Updating system and installing base packages"
     pacman -Sy --noconfirm && \
-    pacman -S --noconfirm --needed base base-devel dialog python && \
+    pacman -S --noconfirm --needed base base-devel dialog python python-pip python-wheel python-setuptools && \
     pacman -Syu --noconfirm && \
     pacman -Sc --noconfirm
     colorecho "Base packages installed"
