c5203c46adffdba6e0095fe801fd02c769bc5d15
0xbbuddha <killian@archimedeos.org>
2026-01-22 21:09:22 +0100

    add depot nihil

--- build/modules/base.sh
+++ build/modules/base.sh
@@ -11,6 +11,19 @@ function package_base() {
     pacman -S --noconfirm --needed base base-devel dialog python python-pip python-wheel python-setuptools && \
     pacman -Syu --noconfirm && \
     pacman -Sc --noconfirm
+    
+    colorecho "Adding Nihil repository to pacman.conf"
+    # Ajouter le dépôt nihil à pacman.conf
+    if ! grep -q "^\[nihil\]" /etc/pacman.conf; then
+        echo "" >> /etc/pacman.conf
+        echo "[nihil]" >> /etc/pacman.conf
+        echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
+        echo "Server = https://TheNullPigeons.github.io/\$arch" >> /etc/pacman.conf
+        colorecho "Nihil repository added to pacman.conf"
+    else
+        colorecho "Nihil repository already exists in pacman.conf"
+    fi
+    
     colorecho "Base packages installed"
 }
 
