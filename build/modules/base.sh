e81d4456833753a60c514a340159f428aaea79b4
0xbbuddha <killian@archimedeos.org>
2026-01-22 20:08:44 +0100

    install python

--- build/modules/base.sh
+++ build/modules/base.sh
@@ -8,7 +8,7 @@ source "${SCRIPT_DIR}/../lib/common.sh"
 function package_base() {
     colorecho "Updating system and installing base packages"
     pacman -Sy --noconfirm && \
-    pacman -S --noconfirm --needed base base-devel dialog && \
+    pacman -S --noconfirm --needed base base-devel dialog python && \
     pacman -Syu --noconfirm && \
     pacman -Sc --noconfirm
     colorecho "Base packages installed"
