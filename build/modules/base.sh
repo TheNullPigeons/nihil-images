865f57b5f5e6ea290c1efa09a2f24e64004b3bb4
0xbbuddha <killian@archimedeos.org>
2026-01-23 10:32:04 +0100

    add zsh and oh-my-zsh

--- build/modules/base.sh
+++ build/modules/base.sh
@@ -8,7 +8,7 @@ source "${SCRIPT_DIR}/../lib/common.sh"
 function package_base() {
     colorecho "Updating system and installing base packages"
     pacman -Sy --noconfirm && \
-    pacman -S --noconfirm --needed base base-devel dialog python python-pip python-wheel python-setuptools && \
+    pacman -S --noconfirm --needed base base-devel dialog python python-pip python-wheel python-setuptools zsh git && \
     pacman -Syu --noconfirm && \
     pacman -Sc --noconfirm
     
@@ -22,6 +22,24 @@ function package_base() {
         colorecho "Nihil repository added to pacman.conf"
     else
         colorecho "Nihil repository already exists in pacman.conf"
+    fi
+
+    # Configure zsh + oh-my-zsh for a nicer shell experience
+    colorecho "Installing and configuring zsh with oh-my-zsh"
+    export ZSH="/root/.oh-my-zsh"
+    if [ ! -d "$ZSH" ]; then
+        git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$ZSH" || colorecho "Failed to clone oh-my-zsh"
+    fi
+
+    if [ ! -f "/root/.zshrc" ] && [ -d "$ZSH" ]; then
+        cp "$ZSH/templates/zshrc.zsh-template" /root/.zshrc || true
+        # Optionally set a nicer theme
+        sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' /root/.zshrc 2>/dev/null || true
+    fi
+
+    # Try to set zsh as default shell for root (non bloquant)
+    if command -v chsh >/dev/null 2>&1; then
+        chsh -s /usr/bin/zsh root || true
     fi
     
     # Synchroniser le dépôt nihil
