ecd884675fb3f034672b698dbdd8bbd47d9ca884
0xbbuddha <killian@archimedeos.org>
2026-01-23 22:05:29 +0100

    more

--- build/modules/install_tools.sh
+++ build/modules/install_tools.sh
@@ -8,7 +8,7 @@ source "${SCRIPT_DIR}/../lib/common.sh"
 function install_tools() {
     colorecho "Updating system and installing tools packages"
     pacman -Sy --noconfirm && \
-    pacman -S --noconfirm --needed nmap openbsd-netcat john sqlmap gobuster && \
+    pacman -S --noconfirm --needed nmap openbsd-netcat john sqlmap gobuster gdb binwalk whois && \
     pacman -Syu --noconfirm && \
     pacman -Sc --noconfirm
 
