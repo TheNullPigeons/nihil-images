02c3ce0247f3fdd858edb3dd3a9d9e4f7d5e7208
0xbbuddha <killian@archimedeos.org>
2026-02-20 10:42:50 +0100

    refactor(modules): extract per tool install function for misc
    [skip ci]

--- build/modules/redteam_misc.sh
+++ build/modules/redteam_misc.sh
@@ -6,6 +6,27 @@ MODULE_DIR="$SCRIPT_DIR"
 source "${SCRIPT_DIR}/../lib/common.sh"
 source "${MODULE_DIR}/../lib/registry/redteam_git.sh"
 
+# ---------------------------------------------------------------------------
+# Individual install functions
+# ---------------------------------------------------------------------------
+
+function install_searchsploit() {
+    local install_dir="/opt/tools/exploitdb"
+    local repo_url="https://gitlab.com/exploit-database/exploitdb.git"
+
+    install_git_tool_symlink "$install_dir" "$repo_url" "searchsploit" || return 1
+    if [ -f "$install_dir/.searchsploit_rc" ]; then
+        cp -n "$install_dir/.searchsploit_rc" ~/.searchsploit_rc
+        sed -i 's/\(.*[pP]aper.*\)/#\1/' ~/.searchsploit_rc
+        sed -i 's|opt/exploitdb|opt/tools/exploitdb|g' ~/.searchsploit_rc
+    fi
+}
+
+
+# ---------------------------------------------------------------------------
+# Module entry point
+# ---------------------------------------------------------------------------
+
 function install_redteam_misc() {
     colorecho "Installing misc red-team tools"
 
@@ -13,18 +34,4 @@ function install_redteam_misc() {
     install_searchsploit
 
     colorecho "Misc red-team tools installation finished"
-}
-
-function install_searchsploit() {
-    local install_dir="/opt/tools/exploitdb"
-    local repo_url="https://gitlab.com/exploit-database/exploitdb.git"
-
-    install_git_tool_symlink "$install_dir" "$repo_url" "searchsploit" || return 1
-
-    # Config searchsploit (spécifique à Exploit-DB)
-    if [ -f "$install_dir/.searchsploit_rc" ]; then
-        cp -n "$install_dir/.searchsploit_rc" ~/.searchsploit_rc
-        sed -i 's/\(.*[pP]aper.*\)/#\1/' ~/.searchsploit_rc
-        sed -i 's|opt/exploitdb|opt/tools/exploitdb|g' ~/.searchsploit_rc
-    fi
 }
